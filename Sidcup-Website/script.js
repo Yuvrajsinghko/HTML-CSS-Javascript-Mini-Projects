126d2b703afc30e48f2c723bfaf881ce13258393
Yuvrajsinghko <yuvrajprince19@gmail.com>
2025-11-04 12:03:01 +0530

    added cards and another section to the website

--- Sidcup-Website/script.js
+++ Sidcup-Website/script.js
@@ -1,10 +1,25 @@
 let crsr = document.querySelector("#cursor");
 let crsrBg = document.querySelector("#cursor-blur");
 document.addEventListener("mousemove", function (event) {
-  crsr.style.left = event.x + "px";
+  crsr.style.left = event.x+30 + "px";
   crsr.style.top = event.y + "px";
   crsrBg.style.left = event.x - 250 + "px";
   crsrBg.style.top = event.y - 250 + "px";
+});
+
+
+let h4all =  document.querySelectorAll("#nav h4");
+h4all.forEach(function(a){
+  a.addEventListener("mouseenter",function(){
+    crsr.style.scale=3;
+    crsr.style.border = "1px solid #fff";
+    crsr.style.backgroundColor ="transparent";
+  });
+  a.addEventListener("mouseleave",function(){
+    crsr.style.scale=1;
+    crsr.style.border = "0px solid #95c11e";
+    crsr.style.backgroundColor ="#95c11e";
+  });
 });
 
 gsap.to("#nav", {
@@ -31,4 +46,7 @@ gsap.to("#main", {
     end: "top -70%",
     scrub: 2,
   },
+});
+gsap.to(crsr,{
+
 });
