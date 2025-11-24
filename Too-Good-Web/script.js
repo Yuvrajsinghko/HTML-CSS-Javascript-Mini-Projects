5e2b4df072f9e2b531db18dfe3fb430493e6a37a
Yuvrajsinghko <yuvrajprince19@gmail.com>
2025-11-24 11:45:54 +0530

    added gsap scroll trigger

--- Too-Good-Web/script.js
+++ Too-Good-Web/script.js
@@ -1,7 +1,59 @@
-const scroll = new LocomotiveScroll({
-  el: document.querySelector("#main"),
-  smooth: true,
-});
+function locomotiveAnimation() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true,
+  });
+  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+  locoScroll.on("scroll", ScrollTrigger.update);
+
+  // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+  ScrollTrigger.scrollerProxy("#main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: document.querySelector("#main").style.transform
+      ? "transform"
+      : "fixed",
+  });
+
+  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+  ScrollTrigger.refresh();
+}
+locomotiveAnimation();
+
+function navAnimation() {
+  gsap.to("#nav-part1 svg", {
+    transform: "translateY(-100%)",
+    scrollTrigger: {
+      trigger: "#page1",
+      scroller: "#main",
+      markers: true,
+      start: "top 0",
+      end: "top -5%",
+      scrub: 1,
+    },
+  });
+}
+
+navAnimation();
 
 function videoConAnimation() {
   let videocom = document.querySelector("#video-container");
@@ -47,38 +99,30 @@ function loadingAnimation() {
 }
 loadingAnimation();
 
-document.addEventListener("mousemove", (events) => {
-  gsap.to("#cursor", {
-    left: events.x,
-    top: events.y,
-  });
-});
-
-// document.querySelectorAll(".child").addEventListener("mouseenter",()=>{
-//   gsap.to("#cursor",{
-//     transform: 'translate(-50%,-50%) scale(1)'
-//   })
-// })
-// document.querySelectorAll(".child").addEventListener("mouseleave",()=>{
-//   gsap.to("#cursor",{
-//     transform: 'translate(-50%,-50%) scale(0)'
-//   })
-// })
-
-let a = document.querySelectorAll(".child");
-
-a.forEach((elem) => {
-  elem.addEventListener("mouseenter", () => {
+function cursorAnimation() {
+  document.addEventListener("mousemove", (events) => {
     gsap.to("#cursor", {
-      transform: "translate(-50%,-50%) scale(1)",
+      left: events.x,
+      top: events.y,
     });
   });
-});
 
-a.forEach((elem) => {
-  elem.addEventListener("mouseleave", () => {
-    gsap.to("#cursor", {
-      transform: "translate(-50%,-50%) scale(0)",
+  let a = document.querySelectorAll(".child");
+
+  a.forEach((elem) => {
+    elem.addEventListener("mouseenter", () => {
+      gsap.to("#cursor", {
+        transform: "translate(-50%,-50%) scale(1)",
+      });
     });
   });
-});
+
+  a.forEach((elem) => {
+    elem.addEventListener("mouseleave", () => {
+      gsap.to("#cursor", {
+        transform: "translate(-50%,-50%) scale(0)",
+      });
+    });
+  });
+}
+cursorAnimation();
